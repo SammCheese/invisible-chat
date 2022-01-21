c4bce065e9585a795aa7b9c8291e8418581f35ff
SammCheese <sammcheese.github@gmail.com>
2022-01-21 06:04:30 +0100

    make inline active indicator update quicker

--- assets/Icons/Button.jsx
+++ assets/Icons/Button.jsx
@@ -1,8 +1,8 @@
 // Some attribution to https://github.com/Sidemen19/send-button
 const {
-    React,
-    getModuleByDisplayName,
-    getModule,
+  React,
+  getModuleByDisplayName,
+  getModule,
 } = require("powercord/webpack");
 
 const Tooltip = getModuleByDisplayName("Tooltip", false);
@@ -10,24 +10,35 @@ const { Button, Icon } = require("powercord/components");
 const buttonClasses = getModule(["button"], false);
 const buttonWrapperClasses = getModule(["buttonWrapper", "pulseButton"], false);
 const buttonTextAreaClasses = getModule(["button", "textArea"], false);
+module.exports = class InvisibleChatButton extends React.Component {
+constructor(props) {
+  super(props);
+}
 
-module.exports = ({ isActive, isEnabled }) => (
+render() {
+  return (
     <Tooltip color="black" postion="top"
-      text={isEnabled ? (isActive ? 'Usage: CoverMessage #!Invisible Message!# #?Password?#' : 'Click to Toggle Encryption') : 'Create Hidden Message'}>
-        {({ onMouseLeave, onMouseEnter }) => (
-            <Button
-                style={{ marginTop: 1 }}
-                look={Button.Looks.BLANK}
-                size={Button.Sizes.ICON}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                <Icon
-                    name="LockClosed"
-                    style={isActive ? { color: "gold" } : { color: "white"}}
-                    className={`${buttonClasses.contents} ${buttonWrapperClasses.button} ${buttonTextAreaClasses.button}`}
-                />
-            </Button>
-        )}
-    </Tooltip>
-)
+    text={this.props.isEnabled ? (this.props.isActive ? 'Usage: CoverMessage #!Invisible Message!# #?Password?#' : 'Click to Toggle Encryption') : 'Create Hidden Message'}>
+      {({ onMouseLeave, onMouseEnter }) => (
+          <Button
+              style={{ marginTop: 1 }}
+              look={Button.Looks.BLANK}
+              size={Button.Sizes.ICON}
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
+              onClick={() => {
+                this.props.isActive = !this.props.isActive;
+              }}
+          >
+              <Icon
+                  id={'invisiblechat-button-bar'}
+                  name="LockClosed"
+                  style={this.props.isActive ? { color: "gold" } : { color: "white"}}
+                  className={`${buttonClasses.contents} ${buttonWrapperClasses.button} ${buttonTextAreaClasses.button}`}
+              />
+          </Button>
+      )}
+  </Tooltip>
+  )
+}
+}

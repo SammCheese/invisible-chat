79329dbc5475796fea53e5e65b6b0f74f7c366ca
Sammy <sammy@sammcheese.net>
2022-03-17 12:03:41 +0100

    Fix Encryption Indicator

--- assets/Icons/MessageIcon.jsx
+++ assets/Icons/MessageIcon.jsx
@@ -2,8 +2,8 @@ const { React } = require("powercord/webpack");
 
 const Lock = React.memo(() => (
     <div style={{ position:'absolute', right: 10, bottom:4 }}>
-        <img src={`https://image.flaticon.com/icons/png/512/3617/3617194.png`}
-        width='16px'
+        <img src={`https://cdn-icons-png.flaticon.com/32/3064/3064130.png`}
+        width='20px'
         />
     </div>
 ));

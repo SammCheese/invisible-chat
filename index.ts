f84459b971a2b8be74f8f1d0898eae52bb9948f2
Albert Portnoy <albert@albertp.dev>
2022-10-19 20:42:21 -0500

    Slightly less janky

--- index.ts
+++ index.ts
@@ -1,18 +1,22 @@
-import type { PluginContext } from "replugged/src/types/entities";
-import type _ from "replugged/src/globals";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type _ from 'replugged/src/globals';
 
-export async function start(ctx: PluginContext<Record<string, unknown>>) {
-  window.replugged.webpack.patchPlaintext([
-    {
-      find: "startTyping",
-      replacements: [
-        {
-          match: /startTyping:.+?,stop/,
-          replace: "startTyping:()=>{},stop",
-        },
-      ],
-    },
-  ]);
+export async function start () {
+  // @ts-expect-error Not a global yet
+  // eslint-disable-next-line no-undef
+  const { webpack, injector } = replugged as Window['replugged'];
+
+  const typingMod = await webpack.waitFor(webpack.byPropsFilter([ 'startTyping' ]));
+  const getChannelMod = await webpack.waitFor(webpack.byPropsFilter([ 'getChannel' ])) as {
+    getChannel: (id: string) => {
+      name: string;
+    }
+  };
+
+  if (typingMod && getChannelMod) {
+    injector.instead(typingMod, 'startTyping', ([ channel ]) => {
+      const channelObj = getChannelMod.getChannel(channel as string);
+      console.log(`Typing prevented! Channel: #${(channelObj?.name ?? 'unknown')} (${channel}).`);
+    });
+  }
 }
-
-export async function stop() {}

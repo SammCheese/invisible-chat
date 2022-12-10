984592b94a5aa697e753db1bcc1f5a27f7387a1d
Albert Portnoy <albert@albertp.dev>
2022-12-10 17:07:11 -0600

    Fix types

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import { webpack, injector } from 'replugged';
+import { AnyFunction } from 'replugged/src/types/util';
 
 const inject = new injector.MiniInjector();
 
 export async function start () {
-  const typingMod = await webpack.waitForModule(webpack.filters.byProps([ 'startTyping' ]));
-  const getChannelMod = await webpack.waitForModule(webpack.filters.byProps([ 'getChannel' ])) as {
+  const typingMod = await webpack.waitForModule(webpack.filters.byProps('startTyping')) as {
+    startTyping: AnyFunction
+  };
+  const getChannelMod = await webpack.waitForModule(webpack.filters.byProps('getChannel')) as {
     getChannel: (id: string) => {
       name: string;
     }

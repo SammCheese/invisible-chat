f6995f3e42b431ea8b0bc098bbca3f06c50df356
Albert Portnoy <albert@albertp.dev>
2022-12-10 20:22:04 -0600

    Copy formatting config from main repo

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,7 @@
 import esbuild from 'esbuild';
-import { globalExternals } from '@fal-works/esbuild-plugin-global-externals';
-import path from 'path';
-import type { Plugin } from 'replugged/src/types/addon';
+import {globalExternals} from '@fal-works/esbuild-plugin-global-externals';
+import path, {join} from 'path';
+import type {Plugin} from 'replugged/src/types/addon';
 import fs from 'fs';
 import _manifest from '../manifest.json';
 
@@ -11,75 +11,119 @@ const NODE_VERSION = '14';
 const CHROME_VERSION = '91';
 
 const globalModules = {
-  replugged: {
-    varName: 'replugged',
-    namedExports: [ 'Injector', 'webpack', 'notices', 'commands', 'settings', 'quickCSS', 'themes', 'ignition', 'plugins' ],
-    defaultExport: true
-  }
+	replugged: {
+		varName: 'replugged',
+		namedExports: [
+			'Injector',
+			'webpack',
+			'notices',
+			'commands',
+			'settings',
+			'quickCSS',
+			'themes',
+			'ignition',
+			'plugins',
+		],
+		defaultExport: true,
+	},
 };
+
+const REPLUGGED_FOLDER_NAME = 'replugged';
+export const CONFIG_PATH = (() => {
+	switch (process.platform) {
+		case 'win32':
+			return join(
+				process.env.APPDATA || '',
+				REPLUGGED_FOLDER_NAME,
+				'plugins',
+			);
+		case 'darwin':
+			return join(
+				process.env.HOME || '',
+				'Library',
+				'Application Support',
+				REPLUGGED_FOLDER_NAME,
+				'plugins',
+			);
+		default:
+			if (process.env.XDG_CONFIG_HOME) {
+				return join(
+					process.env.XDG_CONFIG_HOME,
+					REPLUGGED_FOLDER_NAME,
+					'plugins',
+				);
+			}
+			return join(
+				process.env.HOME || '',
+				'.config',
+				REPLUGGED_FOLDER_NAME,
+				'plugins',
+			);
+	}
+})();
 
 const watch = process.argv.includes('--watch');
 
 const common: esbuild.BuildOptions = {
-  absWorkingDir: path.join(__dirname, '..'),
-  bundle: true,
-  minify: false,
-  sourcemap: true,
-  format: 'cjs' as esbuild.Format,
-  logLevel: 'info',
-  watch
+	absWorkingDir: path.join(__dirname, '..'),
+	bundle: true,
+	minify: false,
+	sourcemap: true,
+	format: 'cjs' as esbuild.Format,
+	logLevel: 'info',
+	watch,
 };
 
 const targets = [];
 
 if ('renderer' in manifest) {
-  targets.push(
-    esbuild.build({
-      ...common,
-      entryPoints: [ manifest.renderer ],
-      platform: 'browser',
-      target: `chrome${CHROME_VERSION}`,
-      outfile: 'dist/renderer.js',
-      format: 'esm' as esbuild.Format,
-      plugins: [ globalExternals(globalModules) ]
-    })
-  );
+	targets.push(
+		esbuild.build({
+			...common,
+			entryPoints: [manifest.renderer],
+			platform: 'browser',
+			target: `chrome${CHROME_VERSION}`,
+			outfile: 'dist/renderer.js',
+			format: 'esm' as esbuild.Format,
+			plugins: [globalExternals(globalModules)],
+		}),
+	);
 
-  manifest.renderer = 'renderer.js';
+	manifest.renderer = 'renderer.js';
 }
 
 if ('preload' in manifest) {
-  targets.push(
-    esbuild.build({
-      ...common,
-      entryPoints: [ manifest.preload ],
-      platform: 'node',
-      target: [ `node${NODE_VERSION}`, `chrome${CHROME_VERSION}` ],
-      outfile: 'dist/preload.js',
-      external: [ 'electron' ]
-    })
-  );
+	targets.push(
+		esbuild.build({
+			...common,
+			entryPoints: [manifest.preload],
+			platform: 'node',
+			target: [`node${NODE_VERSION}`, `chrome${CHROME_VERSION}`],
+			outfile: 'dist/preload.js',
+			external: ['electron'],
+		}),
+	);
 
-  manifest.preload = 'preload.js';
+	manifest.preload = 'preload.js';
 }
 
 if ('main' in manifest) {
-  targets.push(
-    esbuild.build({
-      ...common,
-      entryPoints: [ manifest.main ],
-      platform: 'node',
-      target: `node${NODE_VERSION}`,
-      outfile: 'dist/main.js',
-      external: [ 'electron' ]
-    })
-  );
+	targets.push(
+		esbuild.build({
+			...common,
+			entryPoints: [manifest.main],
+			platform: 'node',
+			target: `node${NODE_VERSION}`,
+			outfile: 'dist/main.js',
+			external: ['electron'],
+		}),
+	);
 
-  manifest.main = 'main.js';
+	manifest.main = 'main.js';
 }
 
 if (!fs.existsSync('dist')) {
-  fs.mkdirSync('dist');
+	fs.mkdirSync('dist');
 }
 
 fs.writeFileSync('dist/manifest.json', JSON.stringify(manifest));
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import {Injector, webpack} from 'replugged';
+import {AnyFunction} from 'replugged/src/types/util';
+
+const inject = new Injector();
+
+export async function start(): Promise<void> {
+	const typingMod = (await webpack.waitForModule(
+		webpack.filters.byProps('startTyping'),
+	)) as {
+		startTyping: AnyFunction;
+	};
+	const getChannelMod = (await webpack.waitForModule(
+		webpack.filters.byProps('getChannel'),
+	)) as {
+		getChannel: (id: string) => {
+			name: string;
+		};
+	};
+
+	if (typingMod && getChannelMod) {
+		inject.instead(typingMod, 'startTyping', ([channel]) => {
+			const channelObj = getChannelMod.getChannel(channel as string);
+			console.log(
+				`Typing prevented! Channel: #${
+					channelObj?.name ?? 'unknown'
+				} (${channel}).`,
+			);
+		});
+	}
+}
+
+export function stop(): void {
+	inject.uninjectAll();
+}

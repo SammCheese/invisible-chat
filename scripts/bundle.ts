3b22d2ad238c72fcd5a0f31169cc7c07a437f047
Albert Portnoy <albert@albertp.dev>
2022-12-14 20:28:27 -0600

    Fix bundling

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -5,4 +5,4 @@ import { Plugin } from "replugged/dist/types/addon";
 const manifest = JSON.parse(readFileSync("manifest.json", "utf-8")) as Plugin;
 const outputName = `${manifest.id}.asar`;
 
-asar.createPackage("../dist", outputName);
+asar.createPackage("dist", outputName);

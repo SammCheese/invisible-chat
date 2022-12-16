96b87213ca3d14dda7e54bb574b60ff237f0a2d1
SammCheese <sammy@sammcheese.net>
2022-12-16 17:06:59 +0100

    Release Alpha 0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
 import { Injector, OutgoingMessage, settings, webpack } from "replugged";
+
 import StegCloak from "./lib/stegcloak.js";
+import { popoverIcon } from "./assets/popoverIcon";
 
 const inject = new Injector();
 
+interface IncomingMessage extends OutgoingMessage {
+  embeds: unknown[];
+  canPin: undefined | boolean;
+}
 interface StegCloak {
   hide: (secret: string, password: unknown, cover: string) => string;
   reveal: (secret: string, password: unknown) => string;
 }
 
-const steggo: StegCloak = new StegCloak(true, false);
-
-const _INV_DETECTION = new RegExp(/( \u200c|\u200d |[\u2060-\u2064])[^\u200b]/);
-
 let activeHotkey = false;
+const steggo: StegCloak = new StegCloak(true, false);
+const INV_DETECTION = new RegExp(/( \u200c|\u200d |[\u2060-\u2064])[^\u200b]/);
+
+export async function start(): Promise<void> {
+  console.log("%c [Invisible Chat] Started!", "color: aquamarine");
+  document.addEventListener("keypress", keypress);
+
+  await injectSendMessages();
+
+  // Register the Message Receiver
+  // @ts-expect-error We are adding to Window
+  window.invisiblechat = {
+    popoverIcon,
+    INV_DETECTION,
+    receiver,
+  };
+}
 
 function keypress(e: KeyboardEvent): void {
   if (e.ctrlKey && e.key === "j") {
@@ -33,24 +52,65 @@ function keypress(e: KeyboardEvent): void {
   }
 }
 
-export async function start(): Promise<void> {
-  console.log("%c [Invisible Chat] Started!", "color: aquamarine");
-  document.addEventListener("keypress", keypress);
-
-  //settings.get('dev.sammcheese.InvisibleChat')
-
-  await injectSendMessages();
-  await injectReceivingMessages();
+export function runPlaintextPatches(): void {
+  webpack.patchPlaintext([
+    {
+      replacements: [
+        {
+          match:
+            /.\?(..)\(\{key:"reply",label:.{1,40},icon:.{1,40},channel:(.{1,3}),message:(.{1,3}),onClick:.{1,5}\}\):null/gm,
+          replace: `$&,$3.content.match(window.invisiblechat.INV_DETECTION)?$1({key:"decrypt",label:"Decrypt Message",icon:window.invisiblechat.popoverIcon,channel:$2,message:$3,onClick:()=>window.invisiblechat.receiver($3)}):null`,
+        },
+        {
+          match: /var .=(.)\.channel,.=.\.message,.=.\.expanded,.=.\.canCopy/gm,
+          replace: `window.invisiblechat.receiver($1.message);$&`,
+        },
+      ],
+    },
+  ]);
 }
 
-async function injectReceivingMessages(): Promise<void> {
-  inject.after(webpack.common.messages, "receiveMessage", (args, res) => {
-    console.log(args, res);
-  });
+// Grab the data from the above Plantext Patches
+function receiver(message: IncomingMessage, canPin: boolean | undefined): void {
+  if (typeof canPin === "undefined") {
+    if (message.content.match(INV_DETECTION) && !message.content.includes("🔒")) {
+      message.content = `🔒${message.content}🔒`;
+    }
+  } else {
+    void buildEmbed(message);
+  }
+}
+
+async function buildEmbed(message: IncomingMessage): Promise<void> {
+  const password =
+    (await settings.get("dev.sammcheese.InvisibleChat").get("defaultPassword")) ?? "password";
+
+  // eslint-disable-next-line no-irregular-whitespace
+  const revealed = steggo.reveal(message.content.replace("​", ""), password);
+
+  let embed = {
+    type: "rich",
+    title: "Decrypted Message",
+    color: "0x45f5f5",
+    description: revealed,
+    footer: {
+      text: "Made with ❤️ by c0dine and Sammy!",
+    },
+  };
+
+  message.embeds.push(embed);
+  updateMessage(message);
   return Promise.resolve();
 }
 
-async function injectSendMessages(): Promise<void> {
+function updateMessage(message: OutgoingMessage): void {
+  webpack.common.fluxDispatcher.dispatch({
+    type: "MESSAGE_UPDATE",
+    message,
+  });
+}
+
+function injectSendMessages(): Promise<void> {
   inject.before(webpack.common.messages, "sendMessage", (args: OutgoingMessage[]) => {
     if (activeHotkey) {
       try {
